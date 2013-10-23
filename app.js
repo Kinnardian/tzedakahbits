5b599336580497ceff2b8b67ca4c3be84bc6671f
Kinnard <khockenhull@gmail.com>
2013-10-23 11:49:54 -0700

    got some basic insert and bitcoin functionality going

--- app.js
+++ app.js
@@ -12,12 +12,6 @@ var pg = require('pg');
 
 
 
-
-
-
-
-
-
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.set('view options', { layout: false });
@@ -41,8 +35,8 @@ app.configure( function(){
 var btcclient = new bitcoin.Client({
   host: 'localhost',
   port: 8332,
-  user: 'username',
-  pass: 'password',
+  user: 'kinnard',
+  pass: 'randomassword',
 
 });
 
@@ -61,32 +55,45 @@ app.get('/newcause', function (req,res){
 })
 
 app.post('/newcause', function (req,res){
-  console.log(req)
-  console.log(req.body);
   
-  var g;
+  console.log(req.body);
   
   var r = [];
   
-  for (g in req.body)
-  {
-    r[g]=req.body[g];
-    console.log('r[g] is ' + r[g]);
-  }
+  r.push(req.body.causeName, req.body.goal, req.body.organization, req.body.sponsor, req.body.submitterEmail);
+  console.log(r);
+
   
-  client = pg.connect(connectionString, function(err, client, done, r){
+  client = pg.connect(connectionString, function(err, client, done){
     if(err) console.log(err);
-    client.query('INSERT INTO causes (cause_name, goal, organization, sponsor, submitter) VALUES ($1,$2,$3,$4,$5)', r, function(err){
-      console.log('This is r' + r)
-      if (err) console.log(err);
+    client.query('INSERT INTO causes (cause_name, goal, organization, sponsor, submitter) VALUES ($1,$2,$3,$4,$5) RETURNING *', r, function(err, result){
+      
+      console.log('This is r ' + r)
+      
+      if (err) return console.log(err);
+      
+      console.log('These are the Rows' + toString(result.rows[0]));
+      btcclient.getNewAddress(function(err,address){
+        if (err) return console.log(err);
+        console.log('New Address' + address);
+        //client.query('INSERT INTO causes VALUES $1')
+        res.render('causepage.jade', {rows: result.rows[0], address: address});
+        return address;
+        
+      });
+
+      console.log('smeh! why is nothing logged' + result.rows[0].cause_id);      
+      
+    
     });    
   });
+  
 });
 
 
 app.get('/causes', function (req,res){
-  res.render('causepage.jade', {title : 'Causes', layout:false})
-})
+  res.render('causepage.jade', {title : 'Causes', layout:false});
+});
 
 app.post('/donatecause', function (req,res){
 
@@ -103,19 +110,19 @@ app.post('/donatecause', function (req,res){
 })
 
 app.get('/donate', function (req,res){
-  res.render('donate.jade', {title : 'Donate'})
-})
+  res.render('donate.jade', {title : 'Donate'});
+});
 
 
 app.get('/bitcoin', function (req,res){
-  res.render('bitcoin.jade', {title : 'What is bitcoin?'})
-})
+  res.render('bitcoin.jade', {title : 'What is bitcoin?'});
+});
 
 app.get('/about', function (req,res){
-  res.render('about.jade', {title : 'About Us'})
-})
+  res.render('about.jade', {title : 'About Us'});
+});
 
 
 
 
-app.listen(4900)
+app.listen(4900);
